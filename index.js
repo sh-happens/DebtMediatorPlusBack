35e5fb5b848e5988be5cce21d782e342429289d6
Yerbol <yerbol_kosbayev@list.ru>
2024-09-30 18:44:11 +0500

    cors

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://www.rfg.kz",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
